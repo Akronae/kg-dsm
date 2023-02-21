6ad0ceebb470bf2f40008fbfbad83231d259914e
Alexandre Daubricourt <alexandre@daubricourt.com>
2023-02-21 14:29:27 +0100

    finished script

--- tokens/scripts/semgen.js
+++ tokens/scripts/semgen.js
@@ -5,7 +5,7 @@ const utils = require("./utils");
 const compspath = process.argv[2];
 if (!compspath)
   throw new Error("No compositions path provided");
-const comps = fs.readFileSync(compspath, "utf8")
+const comps = JSON.parse(fs.readFileSync(compspath, "utf8"))
 
 const semspath = path.dirname(compspath);
 const sems = {}
@@ -14,10 +14,14 @@ for (const file of utils.readdirRecursive(semspath).filter(f => f.endsWith(".jso
   utils.mergeDeep(sems, val)
 }
 
-const usedSemsInCompos = comps.match(/\{sem.*\}/gmi)
+const usedSemsInCompos = Array.from(new Set(JSON.stringify(comps).match(/\{sem.*?\}/gmi)))
 for (const sem of usedSemsInCompos) {
   const name = sem.match(/\{(.*)\}/)[1]
   const semValue = utils.get(sems, name)
-  if (!semValue)
-    console.log(name)
+  if (!semValue) {
+    utils.set(comps, name, {value: '', type: ''})
+    utils.log('Created', utils.yellow(name))
+  }
 }
+
+fs.writeFileSync(compspath, JSON.stringify(comps, null, 2), "utf8")
